8533fdda8a44fbcae615fbe32cd9a3ebf0063ae4
JaiSwarup <jai.swarup.ug22@nsut.ac.in>
2025-01-11 04:17:07 +0530

    improve index

--- src/app.ts
+++ src/app.ts
@@ -6,6 +6,7 @@ import * as middlewares from './middlewares';
 import task from './jobs/cryptoJob';
 import routes from './routes';
 import status from './routes/status';
+import MessageResponse from './interfaces/MessageResponse';
 
 const app = express();
 
@@ -15,6 +16,13 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 task.start();
+
+app.get<{}, MessageResponse>('/', (req, res) => {
+  res.json({
+    message: 'Hello From the API 🚀',
+    data: 'The API is available at /api/v1',
+  });
+});
 app.use('/status', status)
 app.use('/api/v1', routes);
 
